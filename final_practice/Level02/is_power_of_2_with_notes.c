5570956156af54015c6f376b28b1aaef1dacd63d
bw <83955345+itzbw@users.noreply.github.com>
2022-11-17 11:00:39 +0100

    Update is_power_of_2_with_notes.c

--- final_practice/Level02/is_power_of_2_with_notes.c
+++ final_practice/Level02/is_power_of_2_with_notes.c
@@ -3,6 +3,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#include<unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int	    is_power_of_2(unsigned int n)
+{
+    if (n <= 1)
+        return(0);
+    while( n % 2 == 0)
+    {
+        n = n/2;
+        if (n = 1)
+            return (1);
+        return (0);
+    }
+}
+
+
+//TEST//
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+    {
+        printf("%s is power of 2 ?", av[1]);
+        is_power_of_2(atoi(av[1])) ? printf("Yes\n") : printf("No\n");
+        printf("the number is %d:\n", atoi(av[1]));
+    }
+    return (0);
+}
+
+
+
+/*
+
 int	    is_power_of_2(unsigned int n)
 {
     int num = n;
@@ -39,3 +77,4 @@ int	main(int ac, char **av)
     }
     return (0);
 }
+*
