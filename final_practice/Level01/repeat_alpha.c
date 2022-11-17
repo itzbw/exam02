54cbce7f48ddfa4f2425e99182a96d49e69dd10c
bw <83955345+itzbw@users.noreply.github.com>
2022-11-17 09:32:36 +0100

    Update repeat_alpha.c

--- final_practice/Level01/repeat_alpha.c
+++ final_practice/Level01/repeat_alpha.c
@@ -1,29 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwong <bwong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/19 12:29:18 by bwong             #+#    #+#             */
+/*   Updated: 2021/08/20 14:33:29 by bwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+
+int		letter_count(char c)
+{
+	int	repeat;
+
+	if (c >= 'A' && c <= 'Z')
+		repeat = c - 'A' + 1;
+	else if (c >= 'a' && c <= 'z')
+		repeat = c - 'a' + 1;
+	else
+		repeat = 1;
+	return (repeat);
+}
 
 int		main(int ac, char **av)
 {
-	int i;
-	int count;
+	int	repeat;
 
-	i = 0;
-	count = 0;
 	if (ac == 2)
 	{
-		while (av[1][i] != '\0')
+		while (*av[1])
 		{
-			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
-				count = av[1][i] - 64;
-			else if (av[1][i] >= 'a' && av[1][i] <= 'z')
-				count = av[1][i] - 96;
-			while (count)
-			{
-				write(1, &av[1][i], 1);
-				count--;
-			}
-			count = 1;
-			i++;
+			repeat = letter_count(*av[1]);
+			while (repeat--)
+				write(1, av[1], 1);
+			av[1]++;
 		}
 	}
 	write(1, "\n", 1);
 	return (0);
 }
+
