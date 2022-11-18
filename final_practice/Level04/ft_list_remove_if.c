95f63205262b52fcd3cba3310998de42121180b6
bw <83955345+itzbw@users.noreply.github.com>
2022-11-18 15:51:35 +0100

    Create ft_list_remove_if.c

---
+++ final_practice/Level04/ft_list_remove_if.c
@@ -0,0 +1,77 @@
+
+
+
+#include <stdlib.h>
+
+typedef struct      s_list
+{
+    struct s_list   *next;
+    void            *data;
+}                   t_list;
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+    //protect
+    if(*begin_list == 0 || begin_list == 0)
+        return;
+    
+    //assign
+    t_list *current = *begin_list;
+    
+    if(cmp(current->data, data_ref)== 0)
+    {
+        *begin_list = current->next;
+        free(current);
+        return (ft_list_remove_if(begin_list, data_ref, cmp));
+    }
+    ft_list_remove_if(&current->next, data_ref, cmp);
+}
+
+///TEST///
+/*
+#include <stdio.h>
+#include <string.h>
+
+void	print_list(t_list **begin_list)
+{
+	t_list *cur = *begin_list;
+	while (cur != 0)
+	{
+		printf("%p\n", cur->data);
+		cur = cur->next;
+	}
+}
+
+int		main(void)
+{
+	char straa[] = "String aa";
+	t_list *aa = malloc(sizeof(t_list));
+	aa->next = 0;
+	aa->data = straa;
+
+	char strbb[] = "String bb";
+	t_list *bb = malloc(sizeof(t_list));
+	bb->next = 0;
+	bb->data = strbb;
+
+	char strcc[] = "String cc";
+	t_list *cc = malloc(sizeof(t_list));
+	cc->next = 0;
+	cc->data = strcc;
+
+	char strdd[] = "String dd";
+	t_list *dd = malloc(sizeof(t_list));
+	dd->next = 0;
+	dd->data = strdd;
+
+	aa->next = bb;
+	bb->next = cc;
+	cc->next = dd;
+
+	t_list **begin_list = &aa;
+
+	print_list(begin_list);
+	printf("----------\n");
+	ft_list_remove_if(begin_list, strcc, strcmp);
+	print_list(begin_list);
+}*/
