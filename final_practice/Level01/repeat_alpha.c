e0b083a0d20bfa1c3384f5e6f92284b18ab695f0
bw <83955345+itzbw@users.noreply.github.com>
2022-12-03 14:50:33 +0100

    Update repeat_alpha.c

--- final_practice/Level01/repeat_alpha.c
+++ final_practice/Level01/repeat_alpha.c
@@ -1,35 +1,29 @@
-
-
+/*
+if, else if c - 'A'/'a' + 1;
+else repeat = 1
+while repeat-- -> print
+*/
 #include <unistd.h>
 
-int		letter_count(char c)
+int main (int argc, char** argv)
 {
-	int	repeat;
+    int i = 0;
+    int repeat;
 
-	if (c >= 'A' && c <= 'Z')
-		repeat = c - 'A' + 1;
-	else if (c >= 'a' && c <= 'z')
-		repeat = c - 'a' + 1;
-	else
-		repeat = 1;
-	return (repeat);
+    if (argc == 2)
+    {
+        while(argv[1][i])
+        {
+            if(argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+                repeat =  argv[1][i] -'A' + 1;
+            else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+                repeat = argv[1][i] - 'a'+ 1;
+            else
+                repeat = 1;
+            while(repeat--)
+                write(1, &argv[1][i],1);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
 }
-
-int		main(int ac, char **av)
-{
-	int	repeat;
-
-	if (ac == 2)
-	{
-		while (*av[1])
-		{
-			repeat = letter_count(*av[1]);
-			while (repeat--)
-				write(1, av[1], 1);
-			av[1]++;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
-}
-
