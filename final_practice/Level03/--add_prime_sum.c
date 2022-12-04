41405ddc9691a63141337a09083685e9b142ab1e
bw <83955345+itzbw@users.noreply.github.com>
2022-12-04 22:37:23 +0100

    Update --add_prime_sum.c

--- final_practice/Level03/--add_prime_sum.c
+++ final_practice/Level03/--add_prime_sum.c
@@ -10,35 +10,79 @@
     //ft_putnbr(ft_add_prime_sum(ft_atoi(av[1])));
 
 
-int		ft_is_prime(int nb)
+#include <unistd.h>
+
+void putnbr (int nb)
 {
-	int i;
+    int num = nb;
+    char str[] = "0123456789";
 
-	i = 2;
-	while (i < nb)
-	{
-		if (nb % i == 0)
-			return (0);
-		i++;
-	}
-	return (1);
-
+    if (num > 9)
+        putnbr (num / 10);
+    write(1, &str[num%10], 1);
 }
 
-int		ft_add_prime_sum(int nb)
+int ft_atoi (char *str)
 {
-	int i;
-	int prime_sum;
+    int i = 0;
+    int result = 0;
 
-	i = 2;
-	prime_sum = 0;
-	if (nb <= 0)
-		return (0);
-	while (i <= nb)
-	{
-		if (ft_is_prime(i))
-			prime_sum += i;
-		i++;
-	}
-	return (prime_sum);
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+
+    while (str[i])
+    {
+        result = result * 10 + str[i] - '0';
+        i++;
+    }
+     
+    return (result);
+}
+
+
+int isPrime (int nb)
+{
+    int i = 2;
+    int num = nb;
+
+    while(num > i)
+    {
+        if (num % i == 0 )
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
+int add_prime (int nb)
+{
+    int i = 2;
+    int sum = 0;
+
+    if (nb <= 0)
+        return 0;
+
+    while (nb >= i)
+    {
+        if(isPrime (i))
+            sum = sum + i;
+        i++;
+    }
+    return sum;
+}
+
+
+int main (int argc, char** argv)
+{
+    int sum;
+
+    if (argc == 2)
+    {
+        sum = add_prime(ft_atoi(argv[1]));
+        putnbr(sum);
+
+    }
+    else
+        write(1, "0", 1);
+    write (1, "\n", 1);
 }
