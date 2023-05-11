2f6527d6df505d1b424032d750697e028ecf8d8f
bw <83955345+itzbw@users.noreply.github.com>
2023-05-11 14:46:51 +0200

    Update ft_atoi_base.c

--- final_practice/Level03/ft_atoi_base.c
+++ final_practice/Level03/ft_atoi_base.c
@@ -1,10 +1,14 @@
-//ft_atoi
-//space , '-' parsing
-//result *= str_base;
-// 0 to 9 //result += str[i] - '0';
-// A to Z // result += str[i] - 55;
-// a to z // result += str[i] - 87;
+int is_valid (char c, int base)
+{
+    char hex [17] = "0123456789abcdef";
+    char hex_m [17] = "0123456789ABCDEF";
 
+    while (base--)
+        if (hex[base] == c || hex_m[base] == c)
+            return 1;
+
+    return (0);
+}
 
 
 int	ft_atoi_base(const char *str, int str_base)
@@ -13,29 +17,29 @@ int	ft_atoi_base(const char *str, int str_base)
     int sign = 1;
     int result = 0;
 
-    if (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
          i++;
-    while (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-' )
+    if (str[i] == '-')
         {
             sign *= -1;
             i++;
         }
-    }
-    while (str[i])
+    while (is_valid (str[i], str_base))
     {
         result *= str_base;
         if (str[i] >= '0' && str[i] <= '9')
             result += str[i] - '0';
-        else if (str[i] >= 'A' && str[i] <= 'Z')
+        else if (str[i] >= 'A' && str[i] <= 'F')
             result += str[i] - 55;
-        else if (str[i] >= 'a' && str[i] <= 'z')
+        else if (str[i] >= 'a' && str[i] <= 'f')
             result += str[i] - 87;
         i++;
     }
     return (result * sign);
 }
+
+#include <stdio.h>
+#include <stdlib.h>
 
 int	main(int ac, char **av)
 {
