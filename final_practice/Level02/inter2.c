27d36deb44efb7d81a5a7a33107fa2e940b1f661
bw <83955345+itzbw@users.noreply.github.com>
2023-05-15 12:55:29 +0200

    Update inter2.c

--- final_practice/Level02/inter2.c
+++ final_practice/Level02/inter2.c
@@ -6,45 +6,37 @@ int check_double (char *str, char c, int len)
     while (i < len)
     {
         if (str[i] == c)
-            return(0);
+            return 0;
         i++;
     }
-    return (1);
-
+    return 1;
 }
-void ft_inter (char *s1, char *s2)
-{
-    int i = 0;
-    int j;
 
-    while(s1[i])
-    {
-        j = 0;
-        while(s2[j])
-        {
-            if(s1[i] == s2[j])
-            {
-                if (check_double(s1, s1[i], i))
-                {
-                    write(1, &s1[i], 1);
-                    break;
-                }
-            }
-            j++;
-        }
-
-        i++;
-    }
-
-}
 
 int main (int argc, char **argv)
 {
+    int i = 0;
+    int j;
     if (argc == 3)
     {
-        ft_inter(argv[1], argv[2]);
-    }
-    write(1, "\n", 1);
-    return (0);
+        while (argv[1][i])
+        {
+            j = 0;
+            while (argv[2][j])
+            {
+                if (argv[1][i] == argv[2][j])
+                {
+                    if (check_double(argv[1], argv[1][i], i))
+                        write (1, &argv[1][i], 1);
+                        break;
+                }
+                j++;
+            }
+                i++;
+        }
+        
 
+    }
+    write (1, "\n", 1);
+    return 0;
 }
