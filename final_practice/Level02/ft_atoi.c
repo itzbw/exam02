aff95cf86fa6a5cec4d87b7307b80126f0923b33
bw <83955345+itzbw@users.noreply.github.com>
2023-05-21 20:22:34 +0200

    Update ft_atoi.c

--- final_practice/Level02/ft_atoi.c
+++ final_practice/Level02/ft_atoi.c
@@ -11,12 +11,10 @@ int	ft_atoi(const char *str)
      
             while (str[i] <= 32 || str[i] == ' ')
                 i++;
-            while(str[i] == '+' || str[i] == '-')
-            {
-                if (str[i] == '-')
-                    sign *= -1;
-                i++;
-            }
+			if (str[i] == '-')
+        		sign *= -1;
+    		if (str[i] == '+' || str[i] == '-')
+        	i++;
             while (str[i] >= '0' && str[i] <= '9')
             {
                 res = res * 10 + str[i] - '0';
