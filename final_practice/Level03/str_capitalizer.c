47fbb39be9a2aa1dcdd376a607c804276b99c822
bw <83955345+itzbw@users.noreply.github.com>
2023-05-25 13:19:24 +0200

    Update str_capitalizer.c

--- final_practice/Level03/str_capitalizer.c
+++ final_practice/Level03/str_capitalizer.c
@@ -1,42 +1,51 @@
 // str_cap
+	// if i != '\0'
+	// first letter : if a to z: toupper , write , i++;
     //[i] A to Z ->toLower
     //[i] a to z && [i - 1] space / tab  -> toUpper
     //write
 // i < argc -> str-cap , i++
 
 
-#include <unistd.h>
-
-void str_cap(char *str)
+void str_cap (char *str)
 {
     int i = 0;
-	//first letter
-    if (str[i] >= 'a' && str[i] <= 'z')
-		str[i] -= 32;
-	write(1, &str[i], 1);
-    i++;
-	//afterwards
-    while (str[i])
-    {
-        if(str[i] >= 'A' && str[i] <= 'Z')
-            str[i] +=32;
-        if(str[i] >= 'a' && str[i] <= 'z' && str[i - 1]== ' ' || str[i - 1] == '\t')
-            str[i] -=32;
-        write(1, &str[i], 1);
-        i++;
-    }
 
+    if (str[i] != '\0')
+    {
+
+        if (str[i] >= 'a' && str[i] <='z')
+            str[i] -= 32;
+            write (1, &str[i], 1);
+        i++;
+        while (str[i])
+        {
+            if (str[i] >= 'A' && str[i] <='Z')
+                str[i] += 32;
+            if (str[i] >= 'a' && str[i] <='z' && (str[i - 1] == ' ' || str[i - 1]== '\t'))
+                str[i] -= 32;
+            write (1, &str[i], 1);
+            i++;
+        }
+    }
+    write (1, "\n", 1);
 }
+
 
 int main (int argc, char **argv)
 {
     int i = 1;
-	//run through all the arguments
-    while (i < argc)
+
+    if (argc > 1)
     {
-        str_cap(argv[i]);
-        write(1, "\n", 1);
-        i++;
+        while (i < argc)
+        {
+            str_cap (argv[i]);
+            i++;
+
+        }
     }
-    return(0);
+    else 
+        write (1, "\n", 1);
+
 }
